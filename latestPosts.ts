701ca5bddf514bb49ed122e38d3646f424bd0f5d
Michal Veselý <mvesely26@gmail.com>
2024-03-21 11:17:18 +0100

    ...

--- latestPosts.ts
+++ latestPosts.ts
@@ -3,28 +3,3 @@ import Audio from './audio'
 import Video from './video'
 
 export interface latestPosts extends Array<Article|Audio|Video>{}
-
-const posts: latestPosts = [
-    {
-        type: 'clanok',
-        title: '',
-        slug: '',
-        author: {
-            name: '',
-            slug: '',
-        },
-        img: {
-            src: '',
-            credits: ''
-        },
-        date: '',
-        readLength: '',
-        views: 10,
-        shares: 5,
-        summary: '',
-        src: '',
-        credits: '',
-        rating: 5,
-        tags: ['test']
-    }
-]
